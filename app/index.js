551fa5ca61f26c5e20ba20e47700f6260c917a73
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2016-03-24 14:05:57 +0430

    Added the ability to pass an argument as project name

--- app/index.js
+++ app/index.js
@@ -6,18 +6,21 @@ var util = require('util');
 var chalk = require('chalk');
 var mkdirp = require('mkdirp');
 
-
 var LandingpageGenerator = yeoman.Base.extend({
   constructor: function() {
     yeoman.Base.apply(this, arguments);
+
+    // Get app name (project root folder name) via arguments
+    this.argument('appName', { type: String, required: true });
+    this.appname = this.appName;
   },
   initializing: function() {
     console.log('Welcome to the Landing Page Generator');
-    mkdirp.sync('landing');
-    mkdirp.sync('landing/app/css');
-    mkdirp.sync('landing/app/sections');
-    mkdirp.sync('landing/app/css/sass');
-    mkdirp.sync('landing/app/js');
+    mkdirp.sync(this.appName);
+    mkdirp.sync(this.appName + '/app/css');
+    mkdirp.sync(this.appName + '/app/sections');
+    mkdirp.sync(this.appName + '/app/css/sass');
+    mkdirp.sync(this.appName + '/app/js');
   },
   prompting: function() {
     var done = this.async();
@@ -43,36 +46,36 @@ var LandingpageGenerator = yeoman.Base.extend({
   writing: function() {
     this.fs.copy(
       this.templatePath('sass/**'),
-      this.destinationPath('landing/app/css/sass')
+      this.destinationPath(this.appName + '/app/css/sass')
     )
     this.fs.copy(
       this.templatePath('_footer.html'),
-      this.destinationPath('landing/app/footer.html')
+      this.destinationPath(this.appName + '/app/footer.html')
     );
     this.fs.copy(
       this.templatePath('_GruntFile.js'),
-      this.destinationPath('landing/GruntFile.js')
+      this.destinationPath(this.appName + '/GruntFile.js')
     );
     this.fs.copy(
       this.templatePath('_package.json'),
-      this.destinationPath('landing/package.json')
+      this.destinationPath(this.appName + '/package.json')
     );
     this.fs.copy(
       this.templatePath('_bower.json'),
-      this.destinationPath('landing/bower.json')
+      this.destinationPath(this.appName + '/bower.json')
     );
     this.fs.copy(
       this.templatePath('_.bowerrc'),
-      this.destinationPath('landing/.bowerrc')
+      this.destinationPath(this.appName + '/.bowerrc')
     );
     this.fs.copyTpl(
       this.templatePath('_header.html'),
-      this.destinationPath('landing/app/header.html'),
+      this.destinationPath(this.appName + '/app/header.html'),
       { landing_title: this.title }
     );
   },
   install: function() {
-    this.destinationRoot('landing');
+    this.destinationRoot(this.appName);
     this.installDependencies();
   },
   end: function() {
