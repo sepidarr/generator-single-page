d16cb068288382c867aeecb2f79ad655bcd00a9e
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2016-03-25 12:01:41 +0430

    Added the ability to select section and section layout

--- section/index.js
+++ section/index.js
@@ -4,10 +4,40 @@ var yeoman = require('yeoman-generator');
 var _ = require('lodash');
 
 var SectionGenerator = yeoman.Base.extend({
+  _sectionLayouts: {
+    'intro': ['Full', 'Standard', 'Minimal'],
+    'contact': ['Form', 'Readonly'],
+    'how_it_works': ['ImageText', 'Image', 'Text'],
+    'testimonies': ['3 rows, 1 column', '3 rows, 2 columns'],
+    'team': ['Circular', 'Square'],
+    'footer': ['1 row, 1 column', '1 row, 2 columns', '1 row, 3 columns']
+  },
   constructor: function() {
     yeoman.Base.apply(this, arguments);
     this.argument('name', { type: String, required: true });
     this.name = _.camelCase(this.name);
+  },
+  prompting: function() {
+    var done = this.async();
+    var sectionTypePrompts = [{
+      name: 'sectionType',
+      type: 'list',
+      message: 'What section do you need?',
+      choices: ['Intro', 'Contact', 'How It Works', 'Testimonies', 'Team', 'Footer']
+    }];
+    this.prompt(sectionTypePrompts, function( props ) {
+      this.sectionType = props.sectionType;
+      var layoutPropmt = {
+        name: 'sectionLayout',
+        type: 'list',
+        message: 'Select your section layout.',
+        choices: this._sectionLayouts[_.snakeCase(this.sectionType)]
+      }
+      this.prompt(layoutPropmt, function( props ) {
+        this.sectionLayout = props.sectionLayout;
+        done();
+      }.bind(this));
+    }.bind(this));
   },
   initialize: function() {
     this.destinationRoot('app');
