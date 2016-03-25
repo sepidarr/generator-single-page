276831fd9f867943fe65dab70df758b59f1f6874
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2016-03-25 13:34:25 +0430

    Added intro section layouts

--- section/index.js
+++ section/index.js
@@ -8,14 +8,12 @@ var SectionGenerator = yeoman.Base.extend({
     'intro': ['Full', 'Standard', 'Minimal'],
     'contact': ['Form', 'Readonly'],
     'how_it_works': ['ImageText', 'Image', 'Text'],
-    'testimonies': ['3 rows, 1 column', '3 rows, 2 columns'],
+    'testimonies': ['3 rows 1 column', '3 rows 2 columns'],
     'team': ['Circular', 'Square'],
-    'footer': ['1 row, 1 column', '1 row, 2 columns', '1 row, 3 columns']
+    'footer': ['1 row 1 column', '1 row 2 columns', '1 row 3 columns']
   },
   constructor: function() {
     yeoman.Base.apply(this, arguments);
-    // this.argument('name', { type: String, required: true });
-    // this.name = _.camelCase(this.name);
   },
   prompting: function() {
     var done = this.async();
@@ -26,15 +24,15 @@ var SectionGenerator = yeoman.Base.extend({
       choices: ['Intro', 'Contact', 'How It Works', 'Testimonies', 'Team', 'Footer']
     }];
     this.prompt(sectionTypePrompts, function( props ) {
-      this.sectionType = props.sectionType;
+      this.sectionType = _.snakeCase(props.sectionType);
       var layoutPropmt = {
         name: 'sectionLayout',
         type: 'list',
         message: 'Select your section layout.',
-        choices: this._sectionLayouts[_.snakeCase(this.sectionType)]
+        choices: this._sectionLayouts[this.sectionType]
       }
       this.prompt(layoutPropmt, function( props ) {
-        this.sectionLayout = props.sectionLayout;
+        this.sectionLayout = _.snakeCase(props.sectionLayout);
         done();
       }.bind(this));
     }.bind(this));
@@ -42,13 +40,14 @@ var SectionGenerator = yeoman.Base.extend({
   initialize: function() {
     this.destinationRoot('app');
     var context = {
-      id: _.lowerCase(this.sectionType),
-      title: _.capitalize(this.sectionType)
+      id: this.sectionType,
+      title: this.sectionType + ':' + this.sectionLayout
     }
     var fileBase = Date.now() + '_' + this.sectionType;
     var htmlFile = 'sections/' + fileBase + '.html';
+    var templatePath = this.sectionType + '/' + this.sectionLayout + '.html';
 
-    this.template('_section.html', htmlFile, context);
+    this.template(templatePath, htmlFile, context);
   },
   end: function() {
     console.log('SectionGenerator: All done!');
