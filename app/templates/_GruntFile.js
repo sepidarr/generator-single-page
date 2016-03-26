c795c743ce1531c1451081f39213446d99e4e42c
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2016-03-26 13:21:24 +0430

    Added grunt-contrib-uglify

--- app/templates/_GruntFile.js
+++ app/templates/_GruntFile.js
@@ -1,13 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     concat: {
-      dist: {
+      html: {
         src: ["app/header.html", "app/sections/*.html", "app/footer.html"],
         dest: "build/index.html"
       },
-      css: {
+      stylesheets: {
         src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'],
         dest: 'app/css/libs.css'
+      },
+      scripts: {
+        src: [
+          'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js'
+        ],
+        dest: 'app/js/libs.js'
       }
     },
     sass: {
@@ -27,6 +34,13 @@ module.exports = function(grunt) {
 					'build/css/styles.min.css': ['app/css/styles.css']
 				}
       }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'build/js/libs.min.js': ['app/js/libs.js']
+        }
+      }
     }
   });
 
@@ -34,8 +48,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('serve', ['connect']);
-  grunt.registerTask('build', ['concat', 'sass', 'cssmin']);
+  grunt.registerTask('build', ['concat', 'sass', 'cssmin', 'uglify']);
   grunt.registerTask('default', ['build']);
 };
